52d63476f5b940f473e43258e8b726bb99890769
asrms <markoways@gmail.com>
2022-04-01 17:51:31 +0200

    feat: backend working

--- README.md
+++ README.md
@@ -1,2 +1,94 @@
 # Random number generator
 
+
+## Install
+
+npm install
+
+
+
+## usage backend
+
+ts-node main.ts
+
+
+example output:
+{
+  "Bari": [
+    60,
+    39,
+    28,
+    26,
+    6
+  ],
+  "Cagliari": [
+    32,
+    47,
+    71,
+    44,
+    78
+  ],
+  "Firenze": [
+    95,
+    65,
+    28,
+    50,
+    8
+  ],
+  "Genova": [
+    84,
+    62,
+    54,
+    55,
+    23
+  ],
+  "Milano": [
+    8,
+    63,
+    12,
+    78,
+    45
+  ],
+  "Napoli": [
+    17,
+    51,
+    27,
+    95,
+    34
+  ],
+  "Palermo": [
+    32,
+    98,
+    67,
+    87,
+    69
+  ],
+  "Roma": [
+    8,
+    24,
+    76,
+    4,
+    54
+  ],
+  "Torino": [
+    28,
+    98,
+    14,
+    40,
+    78
+  ],
+  "Venezia": [
+    68,
+    47,
+    18,
+    24,
+    59
+  ],
+  "Nazionale": [
+    80,
+    38,
+    75,
+    95,
+    71
+  ]
+}
--- main.ts
+++ main.ts
@@ -1,1 +1,105 @@
-console.log("ciao");
+
+
+
+
+function RNG(min: number,max: number){
+    const rng = Math.random();
+    return Math.trunc(rng * (max - min) + min);
+
+}
+
+function RNGDec(min: number,max: number, precision:number){
+
+    if(precision < 0){
+        throw new Error("il numero deve essere positivo");
+        
+    }
+
+    
+    if(!Number.isInteger(precision)){
+        throw new Error("il numero deve essere intero");
+        
+    }
+
+
+    const rng = Math.random();
+    let n = rng * (max - min) + min;
+      
+    n = n * Math.pow(10, precision);
+    n = Math.floor(n);
+    n = n / Math.pow(10, precision);
+
+
+    return n;
+
+}
+
+function RNGDecCorso(min: number,max: number, precision:number){
+
+    if(precision < 0){
+        throw new Error("il numero deve essere positivo");
+        
+    }
+
+    
+    if(!Number.isInteger(precision)){
+        throw new Error("il numero deve essere intero");
+        
+    }
+
+    const multFactor = Math.pow(10, precision);
+
+
+    return RNG(min * multFactor,max * multFactor) / multFactor;
+
+}
+
+
+
+function RNGSequence(len: number, min: number, max:number){
+    
+    if(len > max - min){
+        throw new Error(`Non posso trovare ${len} numeri tra ${min} e ${max}`);
+    }
+    
+    const res: number[] = [];
+
+    try {
+
+        while(res.length < len){
+            const rn = RNG(min,max);
+    
+            if(res.includes(rn)){
+                continue;
+            }
+            res.push(rn);
+        }
+       return res;  
+    } catch (error) {
+        console.log(error)
+        
+    }
+
+
+}
+
+const ruote = ['Bari','Cagliari','Firenze','Genova','Milano','Napoli','Palermo','Roma','Torino','Venezia','Nazionale'];
+
+const estrazioni: { [ruota: string]: number[]} = {};
+
+for (const ruota of ruote){
+    const estrazione = RNGSequence(5,1,100);
+
+    estrazioni[ruota]= estrazione;
+
+  
+
+}
+
+
+console.log(JSON.stringify(estrazioni, null, 2)); 
+
+/*  console.log(RNGDec(1,5,0)) */
+
+/* console.log(RNGDecCorso(1,5,3)) */
+
